e34bcf1f997f4bc30e99a731dfd290795f3911d7
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-04-25 21:49:20 -0300

    feat: Custom Annotations.

--- src/main/java/com/github/foliveira/SalesConfiguration.java
+++ src/main/java/com/github/foliveira/SalesConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("development")
+@Development
 public class SalesConfiguration {
     @Bean
     public CommandLineRunner executar() {
