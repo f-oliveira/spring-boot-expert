9059b23f99e90cc47fbdf148bd1ee72fe310c32a
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-04-28 14:58:28 -0300

    feat: 3.23 - Query Methods.

--- src/main/java/com/github/foliveira/domain/repository/ClientRepository.java
+++ src/main/java/com/github/foliveira/domain/repository/ClientRepository.java
@@ -1,58 +1,16 @@
 package com.github.foliveira.domain.repository;
 
 import com.github.foliveira.domain.entity.Client;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@Repository
-public class ClientRepository {
 
-    @Autowired
-    private EntityManager entityManager;
+public interface ClientRepository extends JpaRepository<Client, Integer> {
 
-    @Transactional
-    public void save(Client client){
-        entityManager.persist(client);
-    }
+    List<Client> findByNameLike(String name);
 
-    @Transactional
-    public void update(Client client){
-        entityManager.merge(client);
-    }
+    boolean existsByName(String name);
 
-    @Transactional
-    public void delete(Client client){
-        if(!entityManager.contains(client)) {
-            client = entityManager.merge(client);
-        }
-        entityManager.remove(client);
-    }
-
-    @Transactional
-    public void delete(Integer id){
-        Client client = entityManager.find(Client.class, id);
-        delete(client);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Client> searchByName(String name){
-        String jpql = " select c from Client c where c.nome like :nome ";
-        TypedQuery<Client> query =  entityManager.createQuery(jpql, Client.class);
-        query.setParameter("name", "%" + name + "%");
-
-        return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Client> getAll(){
-        return entityManager
-                .createQuery("from client", Client.class)
-                .getResultList();
-    }
 }
