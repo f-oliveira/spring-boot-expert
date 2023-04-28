5c2dbad7ccbd334b81e623c9865767c6cce3cfe6
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-04-28 15:15:16 -0300

    feat: 3.27 - Query methods.

--- src/main/java/com/github/foliveira/domain/repository/ClientRepository.java
+++ src/main/java/com/github/foliveira/domain/repository/ClientRepository.java
@@ -3,13 +3,21 @@ package com.github.foliveira.domain.repository;
 import com.github.foliveira.domain.entity.Client;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
-    List<Client> findByNameLike(String name);
+    @Query(value = "select * from client c where c.name like '%:name%' ", nativeQuery = true)
+    List<Client> encontrarNome(@Param("name") String name);
+
+    @Query(" delete from client c where c.name =:name ")
+    @Modifying
+    void deleteByName(String name);
 
     boolean existsByName(String name);
 
