c5a4328f6e6f576e6ee5d1c48564b04d64a7badb
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-04-30 14:51:14 -0300

    fix: 3.31 - Load orders by costumer.

--- src/main/java/com/github/foliveira/domain/repository/OrderRepository.java
+++ src/main/java/com/github/foliveira/domain/repository/OrderRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
-    List<Order> = findByClient(Client client);
+    List<Order> findByClient(Client client);
 
 }
