27616436f4f480e47f5ac880d98845cdd203eef1
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-05-02 19:54:20 -0300

    feat: 4.39 - Get Request with parameters and Reponse Entity.

--- src/main/java/com/github/foliveira/domain/rest/controller/ClientController.java
+++ src/main/java/com/github/foliveira/domain/rest/controller/ClientController.java
@@ -1,23 +1,31 @@
 package com.github.foliveira.domain.rest.controller;
 
+import com.github.foliveira.domain.entity.Client;
+import com.github.foliveira.domain.repository.ClientRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class ClientController {
 
-    @RequestMapping(
-            value = {"/api/clientes/hello/{nome}", "/api/hello"},
-            method = RequestMethod.GET,
-            consumes = { "application/json", "application/xml" },
-            produces = { "application/json", "application/xml" }
-    )
+    private ClientRepository clientRepository;
+
+    public ClientController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping("api/clients/{id}")
     @ResponseBody
-    public String helloCliente( @PathVariable("nome") String nomeCliente ){
-        return String.format("Hello %s ", nomeCliente);
+    public ResponseEntity getClientById(@PathVariable Integer id){
+        Optional<Client> client =  clientRepository.findById(id);
+
+        if (client.isPresent()) {
+            return ResponseEntity.ok(client.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
